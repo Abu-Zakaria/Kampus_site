8f51e28e81250ff7ffd5f462e8b60afe661cfe52
hasanuzzamanpriyam <h.priyam1999@gmail.com>
2026-09-10 12:07:38 +0600

    feat: create CourseShortlistMail mailable class to send personalized course recommendations

--- app/Mail/CourseShortlistMail.php
+++ app/Mail/CourseShortlistMail.php
@@ -32,7 +32,7 @@ class CourseShortlistMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Your Personalized University Shortlist - Kampus Edu',
+            subject: 'Your Personalized Course Shortlist - Kampus Edu',
         );
     }
 
